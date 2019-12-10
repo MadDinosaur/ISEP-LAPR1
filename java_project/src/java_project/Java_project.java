c887e7ce2be46eb3cb331a5850352bb6670ab90d
Sborges17 <1191053@isep.ipp.pt>
2019-12-10 18:42:45 +0000

    Método para definir períodos, ainda não está acabado.

--- java_project/src/java_project/Java_project.java
+++ java_project/src/java_project/Java_project.java
@@ -3,6 +3,7 @@ package Java_project;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
@@ -15,7 +16,9 @@ public class Java_project {
         LocalDateTime[] dateTime = new LocalDateTime[MAX_OBSERVATIONS];
         int size = readFile(consumptionMW, dateTime);
     } 
-
+    
+    
+    //método para ler ficheiro
     public static int readFile(int[] consumptionMW, LocalDateTime[] dateTime) throws FileNotFoundException {
         Scanner fileScan = new Scanner(new File("DAYTON.csv"));
         fileScan.nextLine(); //descarta a linha do cabeçalho
@@ -32,6 +35,33 @@ public class Java_project {
         fileScan.close();
         //retorna para entrar como comprimento do array
         return numLines;
+    }
+    
+    //escolha do periodo do dia
+    //temporário, ver amanhã com o professor
+    public static void getHour (int []consuptionMW, LocalDateTime[] dateTime) throws FileNotFoundException{
+        double [] Morning = new double [MAX_OBSERVATIONS];
+        
+        for (int i=0;i<dateTime.length;i++){
+            if (dateTime[i].getHour()>=6 && dateTime[i].getHour()<12){
+                    //passar informação toda da linha para array morning
+                
+            }else{
+                 if (dateTime[i].getHour()>=12 && dateTime[i].getHour()<18){
+                    //passar informação toda da linha para array afternoon
+                 
+                 } else {
+                      if (dateTime[i].getHour()>=18&&dateTime[i].getHour()<24){
+                    //passar informação toda da linha para array night
+                
+                 }else{
+                         if (dateTime[i].getHour()>=0&& dateTime[i].getHour()<6){
+                    //passar informação toda da linha para array dawn
+                }
+                         }
+                      }
+            }
+        }
     }
 }
 
