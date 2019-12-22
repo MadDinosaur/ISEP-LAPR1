a2993489bd4fe66e88c466d482eeeec64d907da6
BarbaraDP <1191507@isep.ipp.pt>
2019-12-22 02:33:18 +0000

    Criação de método askUser() para poder executar testes unitários para métodos com Scanner (não completo)

--- 2019-LAPR1_1DK_Mafia/src/pkg2019/lapr1_1dk_mafia/LAPR1_1DK_Mafia.java
+++ 2019-LAPR1_1DK_Mafia/src/pkg2019/lapr1_1dk_mafia/LAPR1_1DK_Mafia.java
@@ -137,14 +137,14 @@ public class LAPR1_1DK_Mafia {
                     case 1:
                         defineAgregacao(option, args);
                         int n = (int) askUser("MMS");
-                        double[] mediaMovelSimples = MediaMovelSimples(consumptionMW, size,n, args, out, agregacao, file);
+                        double[] mediaMovelSimples = MediaMovelSimples(consumptionMW, size, n, args, out, agregacao, file);
                         criarGraficoMediaSimples(consumptionMW, mediaMovelSimples, mediaMovelSimples.length, n, out, args, agregacao, file);
                         absoluteError(consumptionMW, mediaMovelSimples, mediaMovelSimples.length, out, args);
                         break;
                     case 2:
                         defineAgregacao(option, args);
                         double alpha = askUser("MMP");
-                        double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size,alpha, args, out, agregacao, file);
+                        double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size, alpha, args, out, agregacao, file);
                         criarGraficoMediaPesada(consumptionMW, mediaMovelPesada, size, alpha, args, out, agregacao, file);
                         absoluteError(consumptionMW, mediaMovelPesada, mediaMovelPesada.length, out, args);
                         break;
@@ -170,6 +170,7 @@ public class LAPR1_1DK_Mafia {
         }
         return option;
     }
+
     //agrega todos os pedidos de informação ao utilizador (exceto menus) - serve para conduzir testes unitários
     public static double askUser(String methodRequest) {
         switch (methodRequest) {
@@ -183,6 +184,18 @@ public class LAPR1_1DK_Mafia {
                 System.out.println("Insira o valor de α (entre 0 e 1): ");
                 double alpha = sc.nextDouble();
                 return alpha;
+            case "GRAFICO":
+                //Gravar gráficos
+                System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
+                int saveOption = sc.nextInt();
+                return saveOption;
+            case "PREVISAO":
+                //Modelos de previsão
+                System.out.printf("Que tipo de previsão pretende?%n"
+                        + "1. Previsão a partir da média móvel simples;%n"
+                        + "2. Previsão a partir da média exponencialmente pesada.%n");
+                int previsionModel = sc.nextInt();
+                return previsionModel;
         }
         return 0;
     }
@@ -424,14 +437,16 @@ public class LAPR1_1DK_Mafia {
         LocalDateTime date = verifyDate(inputDate, dateTime, size, option, args, out);
         if (date != null) {
             if (date.isAfter(dateTime[size - 1])) {
-                double media = previsionType(consumptionMW, dateTime, size, size - 1, args, out, file);
+                int op = (int) askUser("PREVISAO");
+                double media = previsionType(consumptionMW, dateTime, size, op, size - 1, args, out, file);
                 System.out.println("A previsão de consumo para a data inserida é de " + media + " MW.");
                 if (args.length == 12) {
                     out.println("A previsão de consumo para a data inserida é de " + media + " MW.");
                 }
             } else {
                 long index = searchForDateIndex(dateTime, date, option, args);
-                double media = previsionType(consumptionMW, dateTime, size, (int) index, args, out, file);
+                int op = (int) askUser("PREVISAO");
+                double media = previsionType(consumptionMW, dateTime, size, op, (int) index, args, out, file);
                 System.out.println("A previsão de consumo para a data inserida é de " + media + " MW.");
                 if (args.length == 12) {
                     out.println("A previsão de consumo para a data inserida é de " + media + " MW.");
@@ -552,24 +567,18 @@ public class LAPR1_1DK_Mafia {
     }
 
     //seleciona previsão por média móvel simples ou pesada
-    public static double previsionType(int[] consumptionMW, LocalDateTime[] dateTime, int size, int index, String[] args, PrintWriter out, String file) throws FileNotFoundException {
-        int option = 0;
+    public static double previsionType(int[] consumptionMW, LocalDateTime[] dateTime, int size, int option, int index, String[] args, PrintWriter out, String file) throws FileNotFoundException {
         if (args.length == 2) {
-            System.out.printf("Que tipo de previsão pretende?%n"
-                    + "1. Previsão a partir da média móvel simples;%n"
-                    + "2. Previsão a partir da média exponencialmente pesada.%n");
-
-            option = sc.nextInt();
             switch (option) {
                 case 1:
                     int n = (int) askUser("MMS");
                     double[] mediaMovelSimples = MediaMovelSimples(consumptionMW, size, n, args, out, agregacao, file);
                     return mediaMovelSimples[index];
-                case 2: 
+                case 2:
                     double alpha = askUser("MMP");
-                    double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size,alpha, args, out, agregacao, file);
+                    double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size, alpha, args, out, agregacao, file);
                     return mediaMovelPesada[index];
-                
+
                 default:
                     System.out.println("Opção inválida. ");
                     break;
@@ -578,14 +587,14 @@ public class LAPR1_1DK_Mafia {
             int option2 = Integer.parseInt(args[5]);
             switch (option2) {
                 case 1:
-                    int n = Integer.parseInt(args[9]);                    
-                    double[] mediaMovelSimples = MediaMovelSimples(consumptionMW, size,n, args, out, agregacao, file);
+                    int n = Integer.parseInt(args[9]);
+                    double[] mediaMovelSimples = MediaMovelSimples(consumptionMW, size, n, args, out, agregacao, file);
                     return mediaMovelSimples[index];
-                case 2: 
+                case 2:
                     double alpha = Double.parseDouble(args[9]);
-                    double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size,alpha, args, out, agregacao, file);
+                    double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size, alpha, args, out, agregacao, file);
                     return mediaMovelPesada[index];
-                
+
                 default:
                     break;
 
@@ -857,13 +866,11 @@ public class LAPR1_1DK_Mafia {
         p.newGraph();
         p.plot();
         if (args.length == 2) {
-            System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-            int op = sc.nextInt();
+            int op = (int) askUser("GRAFICO");
 
             if (op != 1 && op != 2 && op != 3 && op != 4) {
                 do {
-                    System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-                    op = sc.nextInt();
+                    op = (int) askUser("GRAFICO");
                 } while (op != 1 && op != 2 && op != 3 && op != 4);
             }
 
@@ -986,13 +993,11 @@ public class LAPR1_1DK_Mafia {
         p.plot();
 
         if (args.length == 2) {
-            System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-            int op = sc.nextInt();
+            int op = (int) askUser("GRAFICO");
 
             if (op != 1 && op != 2 && op != 3 && op != 4) {
                 do {
-                    System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-                    op = sc.nextInt();
+                    op = (int) askUser("GRAFICO");
                 } while (op != 1 && op != 2 && op != 3 && op != 4);
             }
 
@@ -1118,13 +1123,11 @@ public class LAPR1_1DK_Mafia {
         p.newGraph();
         p.plot();
         if (args.length == 2) {
-            System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-            int op = sc.nextInt();
+            int op = (int) askUser("GRAFICO");
 
             if (op != 1 && op != 2 && op != 3 && op != 4) {
                 do {
-                    System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-                    op = sc.nextInt();
+                    op = (int) askUser("GRAFICO");
                 } while (op != 1 && op != 2 && op != 3 && op != 4);
             }
 
@@ -1237,13 +1240,11 @@ public class LAPR1_1DK_Mafia {
         p.newGraph();
         p.plot();
         if (args.length == 2) {
-            System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-            int op = sc.nextInt();
+            int op = (int) askUser("Grafico");
 
             if (op != 1 && op != 2 && op != 3 && op != 4) {
                 do {
-                    System.out.println("Pretende gravar o gráfico? 1.PNG 2.CSV 3.PNG e CSV 4.Não");
-                    op = sc.nextInt();
+                    op = (int) askUser("Grafico");
                 } while (op != 1 && op != 2 && op != 3 && op != 4);
             }
 
@@ -1380,6 +1381,60 @@ public class LAPR1_1DK_Mafia {
         out.close();
     }
 
+    public static void defineAgregacao(int option, String[] args) {
+        if (args.length == 2) {
+            switch (option) {
+                case 1:
+                    agregacao = "Madrugadas";
+                    break;
+                case 2:
+                    agregacao = "Manhas";
+                    break;
+                case 3:
+                    agregacao = "Tardes";
+                    break;
+                case 4:
+                    agregacao = "Noites";
+                    break;
+                case 5:
+                    agregacao = "Diario";
+                    break;
+                case 6:
+                    agregacao = "Mensal";
+                    break;
+                case 7:
+                    agregacao = "Anual";
+                    break;
+            }
+        } else {
+            int option2 = Integer.parseInt(args[3]);
+            switch (option2) {
+                case 11:
+                    agregacao = "Madrugadas";
+                    break;
+                case 12:
+                    agregacao = "Manhas";
+                    break;
+                case 13:
+                    agregacao = "Tardes";
+                    break;
+                case 14:
+                    agregacao = "Noites";
+                    break;
+                case 2:
+                    agregacao = "Diario";
+                    break;
+                case 3:
+                    agregacao = "Mensal";
+                    break;
+                case 4:
+                    agregacao = "Anual";
+                    break;
+            }
+
+        }
+    }
+
     //--------------------------------------------NAO INTERATIVO--------------------------------------------------------
     private static void DefinePeriodNonInteractive(int[] consumptionMW, LocalDateTime[] dateTime, int size, String[] args, PrintWriter out, String file) throws FileNotFoundException {
         int option = 0; //inicializar o option, que não vai ser necessário
@@ -1473,7 +1528,7 @@ public class LAPR1_1DK_Mafia {
                 break;
             case "2":
                 double alpha = Double.parseDouble(args[9]);
-                double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size,alpha, args, out, agregacao, file);
+                double[] mediaMovelPesada = MediaMovelPesada(consumptionMW, size, alpha, args, out, agregacao, file);
                 criarGraficoMediaPesada(consumptionMW, mediaMovelPesada, size, alpha, args, out, agregacao, file);
                 absoluteError(consumptionMW, mediaMovelPesada, mediaMovelPesada.length, out, args);
                 break;
@@ -1481,60 +1536,6 @@ public class LAPR1_1DK_Mafia {
                 System.out.println("Parâmetro de modelo inválido.");
                 out.println("Parâmetro de modelo inválido.");
                 break;
-        }
-    }
-
-    public static void defineAgregacao(int option, String[] args) {
-        if (args.length == 2) {
-            switch (option) {
-                case 1:
-                    agregacao = "Madrugadas";
-                    break;
-                case 2:
-                    agregacao = "Manhas";
-                    break;
-                case 3:
-                    agregacao = "Tardes";
-                    break;
-                case 4:
-                    agregacao = "Noites";
-                    break;
-                case 5:
-                    agregacao = "Diario";
-                    break;
-                case 6:
-                    agregacao = "Mensal";
-                    break;
-                case 7:
-                    agregacao = "Anual";
-                    break;
-            }
-        } else {
-            int option2 = Integer.parseInt(args[3]);
-            switch (option2) {
-                case 11:
-                    agregacao = "Madrugadas";
-                    break;
-                case 12:
-                    agregacao = "Manhas";
-                    break;
-                case 13:
-                    agregacao = "Tardes";
-                    break;
-                case 14:
-                    agregacao = "Noites";
-                    break;
-                case 2:
-                    agregacao = "Diario";
-                    break;
-                case 3:
-                    agregacao = "Mensal";
-                    break;
-                case 4:
-                    agregacao = "Anual";
-                    break;
-            }
-
         }
     }
 }
